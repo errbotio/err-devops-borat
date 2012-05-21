5b589d6ae12690f3e76f33d229a8532c844f0ec1
Guillaume BINET <gbin@gootz.net>
2012-05-21 15:15:59 +0200

    init commit

---
+++ README.rst
@@ -0,0 +1,22 @@
+devops_borat - random funny quotes about development
+====================================================
+
+For more information about err you can find it here: https://github.com/gbin/err
+
+**Installation**
+
+Dependencies::
+
+    pip install feedparser
+
+If you have the admin rights on an err chatbot simply use::
+
+    !install git@github.com:gbin/err-devops_borat.git
+
+in order to install the plugin.
+Then !help to see the available commands and their explanation.
+
+**Credits**
+
+Vincent Alsteen https://twitter.com/#!/_v1nc3nt_
+
---
+++ devops_borat.py
@@ -0,0 +1,23 @@
+from random import choice
+from feedparser import parse
+from botplugin import BotPlugin
+from jabberbot import botcmd
+
+class DevOpsBorat(BotPlugin):
+    @botcmd
+    def borat(self, mess, args):
+        myfeed = parse('http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=DEVOPS_BORAT')
+        items = myfeed['entries']
+        return choice(items).description
+
+    @botcmd
+    def jesus(self, mess, args):
+        myfeed = parse('http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=devops_jesus')
+        items = myfeed['entries']
+        return choice(items).description
+
+    @botcmd
+    def yoda(self, mess, args):
+        myfeed = parse('http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=UXYoda')
+        items = myfeed['entries']
+        return choice(items).description
