061a2af4a395fd313436323275c311a5f65694af
Guillaume BINET <gbin@gootz.net>
2012-08-18 15:28:14 +0200

    interim code for 1.6.0

--- devops_borat.py
+++ devops_borat.py
@@ -1,7 +1,13 @@
 from random import choice
 from feedparser import parse
-from errbot.botplugin import BotPlugin
-from errbot.jabberbot import botcmd
+# Backward compatibility
+from errbot.version import VERSION
+from errbot.utils import version2array
+if version2array(VERSION) >= [1,6,0]:
+    from errbot import botcmd, BotPlugin
+else:
+    from errbot.botplugin import BotPlugin
+    from errbot.jabberbot import botcmd
 
 class DevOpsBorat(BotPlugin):
     """
